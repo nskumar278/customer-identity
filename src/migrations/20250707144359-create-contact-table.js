9b12e67ee19cfe69fabd24a2247214530b61c8c3
Sampath Kumar <sampathkumar.n@rently-Latitude-5420>
2025-07-07 20:45:42 +0530

    added migration to create contact table

---
+++ src/migrations/20250707144359-create-contact-table.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Contacts', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      phoneNumber: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+      linkedId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      linkedPrecedence: {
+        type: Sequelize.ENUM('primary', 'secondary'),
+        defaultValue: 'primary',
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Contacts');
+  }
+};
